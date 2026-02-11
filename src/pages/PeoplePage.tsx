50fb5f75992dd883abf78fc29814f19a6810fcc9
Adelina Yovdii <adelincaiovdii@gmail.com>
2026-02-12 00:50:39 +0200

    solution

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -7,7 +7,7 @@ import { PeopleFilters } from '../components/PeopleFilters';
 import type { Person } from '../types/Person';
 
 export const PeoplePage = () => {
-  const [people, setPeople] = useState<Person[]>([]);
+  const [people, setPeople] = useState<Person[] | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -17,6 +17,7 @@ export const PeoplePage = () => {
   useEffect(() => {
     setError(false);
     setLoading(true);
+    setPeople(null);
 
     fetch('https://mate-academy.github.io/react_people-table/api/people.json')
       .then(res => res.json())
@@ -31,6 +32,9 @@ export const PeoplePage = () => {
   }, []);
 
   const filteredPeople = useMemo(() => {
+    if (!people)
+      return [];
+
     let result = [...people];
 
     const query = searchParams.get('query')?.toLowerCase() || '';
@@ -42,7 +46,7 @@ export const PeoplePage = () => {
         p =>
           p.name.toLowerCase().includes(query) ||
           p.motherName?.toLowerCase().includes(query) ||
-          p.fatherName?.toLowerCase().includes(query),
+          p.fatherName?.toLowerCase().includes(query)
       );
     }
 
@@ -53,7 +57,6 @@ export const PeoplePage = () => {
     if (centuries.length) {
       result = result.filter(p => {
         const century = Math.ceil(p.born / 100).toString();
-
         return centuries.includes(century);
       });
     }
@@ -66,7 +69,7 @@ export const PeoplePage = () => {
       <h1 className="title">People Page</h1>
 
       <div className="columns is-desktop is-flex-direction-row-reverse">
-        {!loading && !error && people.length > 0 && (
+        {!loading && !error && people && people.length > 0 && (
           <div className="column is-7-tablet is-narrow-desktop">
             <PeopleFilters />
           </div>
@@ -82,18 +85,19 @@ export const PeoplePage = () => {
               </p>
             )}
 
-            {!loading && !error && people.length === 0 && (
+            {!loading && !error && people && people.length === 0 && (
               <p data-cy="noPeopleMessage">There are no people on the server</p>
             )}
 
             {!loading &&
               !error &&
+              people &&
               people.length > 0 &&
               filteredPeople.length === 0 && (
                 <p>There are no people matching the current search criteria</p>
               )}
 
-            {!loading && !error && filteredPeople.length > 0 && (
+            {!loading && !error && people && filteredPeople.length > 0 && (
               <PeopleTable people={filteredPeople} selectedSlug={slug} />
             )}
           </div>
